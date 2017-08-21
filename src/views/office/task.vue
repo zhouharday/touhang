d0fa7f01c565469d3e5f52cc3be38bb7c62a20ed
harday <719588366@qq.com>
2017-08-21 19:14:01 +0800

    21/8

--- src/views/office/task.vue
+++ src/views/office/task.vue
@@ -9,91 +9,119 @@
                 <Tab-pane label="代办任务">代办任务</Tab-pane>
                 <Tab-pane label="已办任务">已办任务</Tab-pane>
             </Tabs>
-            <el-button type="text" @click="dialogFormVisible = true">
+            <el-button type="text" @click="dialogFormVisibles">
                 <span>添加任务</span>
             </el-button>
             <el-dialog title="添加任务" :visible.sync="dialogFormVisible" class="taskDialog_title">
-                <!-- <el-form :model="form">
-                            <el-row :gutter="20">
-                                <el-col :span="8">
-                                    <el-form-item label="活动名称" :label-width="formLabelWidth">
-                                        <el-input v-model="form.name" class="el_input" auto-complete="off"></el-input>
-                                    </el-form-item>
-                                </el-col>
-                                <el-col :span="8">
-                                    <el-form-item label="活动名称" :label-width="formLabelWidth">
-                                        <el-input v-model="form.name" class="el_input" auto-complete="off"></el-input>
-                                    </el-form-item>
-                                </el-col>
-                                <el-col :span="8">
-                                    <el-form-item label="活动名称" :label-width="formLabelWidth">
-                                        <el-input v-model="form.name" class="el_input" auto-complete="off"></el-input>
-                                    </el-form-item>
-                                </el-col>
-                            </el-row>
-                        </el-form> -->
-                <el-table :data="tableData" border style="width: 100%">
-                    <el-table-column prop="date" label="日期" width="180">
-                    </el-table-column>
-                    <el-table-column prop="name" label="姓名" width="180">
-                        <template scope="scope">
-                            <input type="text" v-model="scope.row.name">
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="address" label="地址">
-                        <template scope="scope">
-                            <input type="text" v-model="scope.row.address">
-                        </template>
-                    </el-table-column>
-                </el-table>
+                <el-form :label-position="labelPosition" :model="form">
+                    <el-row :gutter="20">
+                        <el-col :span="24">
+                            <el-form-item label="任务名称" :label-width="formLabelWidth">
+                                <el-input v-model="form.name" class="el_input" auto-complete="off"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <!-- <el-col :span="12">
+                                            <el-form-item label="开始时间" :label-width="formLabelWidth">
+                                                <el-input v-model="form.name" class="el_input" auto-complete="off"></el-input>
+                                            </el-form-item>
+                                        </el-col> -->
+                    </el-row>
+                    <el-row :gutter="20">
+                        <el-col :span="12">
+                            <el-form-item label="开始时间" :label-width="formLabelWidth">
+                                <el-input v-model="form.startDate" class="el_input" auto-complete="off"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label="完成时间" :label-width="formLabelWidth">
+                                <el-date-picker v-model="form.switchDate" type="datetime" placeholder="选择日期时间" align="right" :picker-options="pickerOptions1">
+                                </el-date-picker>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20">
+                        <el-col :span="12">
+                            <el-form-item label="接收人" :label-width="formLabelWidth">
+                                <el-input v-model="form.pople1" class="el_input" auto-complete="off"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label="指派人" :label-width="formLabelWidth">
+                                <el-input v-model="form.pople2" class="el_input" auto-complete="off"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20">
+                        <el-col :span="24">
+                            <el-form-item label="任务说明" :label-width="formLabelWidth">
+                                <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="textContent">
+                                </el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                </el-form>
+                <!-- <el-table :data="tableData" border style="width: 100%">
+                                        <el-table-column prop="date" label="日期" width="180">
+                                        </el-table-column>
+                                        <el-table-column prop="name" label="姓名" width="180">
+                                            <template scope="scope">
+                                                <input type="text" v-model="scope.row.name">
+                                            </template>
+                                        </el-table-column>
+                                        <el-table-column prop="address" label="地址">
+                                            <template scope="scope">
+                                                <input type="text" v-model="scope.row.address">
+                                            </template>
+                                        </el-table-column>
+                                    </el-table> -->
                 <div slot="footer" class="dialog-footer">
                     <el-button @click="dialogFormVisible = false">取 消</el-button>
                     <el-button type="primary" @click="tabValue()">确 定</el-button>
                 </div>
             </el-dialog>
             <div>
-                
+
             </div>
         </div>
 
-        <div v-show="dialogVisible" class="shadowBox">
-            <div>
-                <div>
-                    <!-- Table -->
-                    <table cellspacing="0" border="1">
-                        <tr>
-                            <td colspan="4">添加任务</td>
+        <!-- <div v-show="dialogVisible" class="shadowBox">
+                        <div>
+                            <div>
+                                <!-- Table -->
+        <!-- <table cellspacing="0" border="1">
+                                    <tr>
+                                        <td colspan="4">添加任务</td>
 
-                        </tr>
-                        <tr>
-                            <td>任务名称</td>
-                            <td colspan="3">
-                                <input>
-                            </td>
+                                    </tr>
+                                    <tr>
+                                        <td>任务名称</td>
+                                        <td colspan="3">
+                                            <input>
+                                        </td>
 
-                        </tr>
-                        <tr>
-                            <td>开始时间</td>
-                            <td><input></td>
-                            <td>完成时间</td>
-                            <td><input></td>
-                        </tr>
-                        <tr>
-                            <td>任务说明</td>
-                            <td colspan="3"><input></td>
-                        </tr>
-                        <tr>
-                            <td>接收人</td>
-                            <td><input></td>
-                            <td>指派人</td>
-                            <td><input></td>
-                        </tr>
-                    </table>
-                    <el-button>保存</el-button>
-                    <el-button>取消</el-button>
-                </div>
-            </div>
-        </div>
+                                    </tr>
+                                    <tr>
+                                        <td>开始时间</td>
+                                        <td><input></td>
+                                        <td>完成时间</td>
+                                        <td><input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>任务说明</td>
+                                        <td colspan="3"><input></td>
+                                    </tr>
+                                    <tr>
+                                        <td>接收人</td>
+                                        <td><input></td>
+                                        <td>指派人</td>
+                                        <td><input></td>
+                                    </tr>
+                                </table>
+                                <el-button>保存</el-button>
+                                <el-button>取消</el-button>
+                            </div>
+                        </div>
+                    </div> -->
     </section>
 </template>
 
@@ -170,6 +198,7 @@ section {
 export default {
     data() {
         return {
+            labelPosition: "left",
             tableData: [{
                 date: '项目名称',
                 name: '',
@@ -187,18 +216,15 @@ export default {
                 name: '',
                 address: ''
             }],
-            dialogTableVisible: false,
             dialogFormVisible: false,
             dialogVisible: false,
             form: {
                 name: '',
-                region: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
-                desc: ''
+                startDate: '',
+                switchDate: '',
+                pople1: '',
+                pople2: '',
+                textContent: ''
             },
             formLabelWidth: '68px',
             columns1: [
@@ -238,14 +264,60 @@ export default {
                     align: "center"
                 }
             ],
-        }
+            pickerOptions1: {
+                shortcuts: [{
+                    text: '今天',
+                    onClick(picker) {
+                        picker.$emit('pick', new Date());
+                    }
+                }, {
+                    text: '昨天',
+                    onClick(picker) {
+                        const date = new Date();
+                        date.setTime(date.getTime() - 3600 * 1000 * 24);
+                        picker.$emit('pick', date);
+                    }
+                }, {
+                    text: '一周前',
+                    onClick(picker) {
+                        const date = new Date();
+                        date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                        picker.$emit('pick', date);
+                    }
+                }]
+            },
+        };
     },
+
     methods: {
-        tabValue(){
-            this.tableData.forEach(function(item) {
+        tabValue() {
+            this.tableData.forEach(function (item) {
                 this.dialogFormVisible = false;
                 console.log(item.name);
             });
+        },
+        getDate() {//封装获取当前系统时间的方法
+            // this.dialogFormVisible = true;
+            var date = new Date();
+            var seperator1 = "-";
+            var seperator2 = ":";
+            var month = date.getMonth() + 1;
+            var strDate = date.getDate();
+            if (month >= 1 && month <= 9) {
+                month = "0" + month;
+            }
+            if (strDate >= 0 && strDate <= 9) {
+                strDate = "0" + strDate;
+            }
+            var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+                + " " + date.getHours() + seperator2 + date.getMinutes()
+                + seperator2 + date.getSeconds();
+            return currentdate;
+        },
+        dialogFormVisibles(){
+            this.dialogFormVisible = true;
+            let startDate = this.getDate();//获取当前系统时间
+            this.form.startDate = startDate;
         }
     }
 }
