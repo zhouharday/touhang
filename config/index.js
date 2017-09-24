141c92b9ed2a71c4f5764693886280c9c83a6306
a <111111111@qq.com>
2017-09-24 22:46:59 +0800

    update

--- config/index.js
+++ config/index.js
@@ -32,8 +32,8 @@ module.exports = {
         proxyTable: {
             '/api': {
                 // target:'http://192.168.0.88:9091',
-                target: 'http://18f46963j4.iok.la',
-                // target:'http://192.168.0.198:9091',
+                // target: 'http://18f46963j4.iok.la',
+                target:'http://192.168.0.198:9091',
                 //  target:'http://192.168.0.118:9091',
                 // target:'http://192.168.0.117:9091',
                 // target:'http://100.64.33.181:9091',
