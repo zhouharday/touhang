5f92ce1082c6df857103f8cff9a465b734b5fea1
zh-harday <zhouhui001@chinasofti.com>
2017-09-30 09:39:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -25,9 +25,9 @@ Vue.use(echarts);
 Vue.use(ElementUI);
 Vue.prototype.md5 = md5;
 Vue.prototype.$http = axios;
-// Vue.prototype.api = '/api'; //公司IP请求时 URL
+Vue.prototype.api = '/api'; //公司IP请求时 URL
 Vue.prototype._ = _ // lodash.js
-Vue.prototype.api = 'http://18f46963j4.iok.la'; //外网请求时 URL
+// Vue.prototype.api = 'http://18f46963j4.iok.la'; //外网请求时 URL
 // Vue.prototype.api = ''; //上线时 URL
 // Vue.prototype.api = process.env.NODE_ENV === 'production'? "" : "/api";
 // Vue.prototype.$http = axios.create();
