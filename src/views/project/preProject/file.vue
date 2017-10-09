020311eacf6b188b13393b1fd5fda6465993724b
cj <cj635881197@126.com>
2017-10-09 22:05:47 +0800

    CJ 项目详情-文档
    CJ 项目详情-文档 2017-10-9 22:05:43

--- src/views/project/preProject/file.vue
+++ src/views/project/preProject/file.vue
@@ -20,27 +20,53 @@
                     <span>操作</span>
                 </div>
             </div>
-            <el-table :data="fileData" v-for="docList in projectDocList" border style="width: 100%" align="center">
-                <v-for="docItem in docList.dataDocumentResult">
-                <el-table-column label="docList.title" prop="docItem.allocationDocumentName" align="center">
-                </el-table-column>
-                <el-table-column prop="docItem.documentName" align="center">
-                </el-table-column>
-                <el-table-column prop="docItem.uploadUser" align="center">
-                </el-table-column>
-                <el-table-column prop="docItem.createDate" align="center">
-                </el-table-column>
-                <el-table-column align="center">
-                    <template scope="scope">
-                        <a href="/static/img/templet.txt" style="font-size:12px;" download="xxxxx文档">下载</a>
-                        <el-button type="text" size="small" class="btn_border" @click="preview(scope.row)">预览</el-button>
-                        <el-button type="text" size="small" @click="handleDelete(scope.$index,fileData)">删除</el-button>
-                    </template>
-                </el-table-column>
-            </v-for>
-            </el-table>
+            <template v-for="(docList, index) in projectDocList">
+                <el-table :data="docList.dataDocumentResult" border style="width: 100%" align="center">
+                    <el-table-column :label="docList.title" prop="allocationDocumentName" align="center">
+                    </el-table-column>
+                    <el-table-column prop="documentName" align="center">
+                    </el-table-column>
+                    <el-table-column prop="uploadUser" align="center">
+                    </el-table-column>
+                    <el-table-column prop="createDate" align="center">
+                    </el-table-column>
+                    <el-table-column align="center">
+                        <template scope="scope">
+                            <el-button v-if="index != 0" type="text" size="small" class="border_right" @click="projectDialog">上传</el-button>
+                            <a v-if="id != '' && id != null" href="documentUrl" style="font-size:12px;" download="xxxxx文档">下载</a>
+                            <el-button v-if="id != '' && id != null" type="text" size="small" class="btn_border" @click="preview(scope.row)">预览</el-button>
+                            <el-button v-if="id != '' && id != null" type="text" size="small" @click="handleDelete(scope.$index,fileData)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </v-for>
+                </el-table>
+            </template>
+            <!-- 上传文档 对话框-->
+            <el-dialog title="上传文档" :visible.sync="modalAdd3" :close-on-click-modal="false">
+                <el-form :model="projectForm" :label-width="formLabelWidth">
+                    <el-form-item label="上传人">
+                        <el-input v-model="projectForm.user" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="上传时间">
+                        <el-date-picker type="date" placeholder="选择日期" v-model="projectForm.date" style="width: 100%;">
+                        </el-date-picker>
+                    </el-form-item>
+                    <el-form-item label="上传文件">
+                        <!-- action 上传的地址，必填 -->
+                        <Upload multiple type="drag" :before-upload="handleUpload" action="//jsonplaceholder.typicode.com/posts/">
+                            <div style="padding: 20px 0">
+                                <Icon type="ios-cloud-upload" size="52"></Icon>
+                                <p>点击或将文件拖拽到这里上传</p>
+                            </div>
+                        </Upload>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button type="default"  @click="modalAdd3 = false">取 消</el-button>
+                    <el-button type="danger"  @click="upload" :loading="loadingStatus">{{ loadingStatus ? '上传中' : '点击上传' }}</el-button>
+                </div>
+            </el-dialog>
         </div>
-
         <!-- 文件预览功能 -->
         <div class="viewFiles" v-show="isHide">
             <div class="closeView" @click="closeView">
@@ -129,8 +155,6 @@ export default {
         getProjectDocument() {
             getProjectDoc(this.projectId).then(resp => {
                 this.projectDocList = resp.data.result || [];
-                this.projectDocList.push();
-                console.log("projectDocList" + JSON.stringify(this.projectDocList));
             }).catch(e => {
                 console.log('getPreDetail() exists error: ', e);
             });
