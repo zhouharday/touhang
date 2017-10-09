eef5590b7d90421a0bd3bca9aa71cfbc0a911c4a
huiliaoliao <1020367243@qq.com>
2017-10-09 18:00:02 +0800

    update
    tuo

--- src/views/system/createForm.vue
+++ src/views/system/createForm.vue
@@ -1,43 +1,53 @@
 <template>
-    <section class="createForm">
-        <div style="background-color: #F2F4F8">
-            <div class="button" >
+    <section class="createForm" style="height: 100%">
+        <el-row :gutter="20" style="margin:0;background-color: #F2F4F8;">
+            <div class="button" style="margin-bottom: 20px">
                 <el-button type="danger" size="small">保存</el-button>
                 <el-button type="danger" size="small">预览</el-button>
                 <el-button type="danger" size="small">取消</el-button>
             </div>
-            <div class="clear"></div>
-            <el-row>
-                <el-col :span="5" style="background-color: #fff">
-                    <div class="form-components">
-                        <span>表单组件</span>
-                        <ul>
-                            <li v-for="list in btnLists">
-                                <el-button>{{list.data}}</el-button>
-                            </li>
-                        </ul>
-                    </div>
-                </el-col>
-                <el-col :span="14" style="background-color: #fff;height: 100%">
-                    <div class="form-components">中间</div>
-                </el-col>
-                <el-col :span="5" style="background-color: #fff">
-                    <div class="form-components">
-                        <span>组件设置</span>
-                        <ul>
-                            <li prop="标题">
-                                标题
-                                <el-input></el-input>
-                            </li>
-                            <li prop="必填项目">
-                                <el-checkbox>这个是必填项</el-checkbox>
-                            </li>
-                        </ul>
-
-                    </div>
-                </el-col>
-            </el-row>
-        </div>
+        </el-row>
+        <el-row :gutter="20"
+                style="margin:0;background-color:#F2F4F8;height:100%;box-sizing: border-box;overflow: hidden">
+            <el-col :span="5" style="background-color: #fff;height: 100%;margin-left: -10px">
+                <div class="form-components">
+                    <span>表单组件</span>
+                    <ul>
+                        <li v-for="list in btnLists">
+                            <el-button>{{list.data}}</el-button>
+                        </li>
+                    </ul>
+                </div>
+            </el-col>
+            <el-col :span="14" style="background-color: #fff;height: 100%;margin: 0 10px;">
+                <div class="form-components">中间内容</div>
+            </el-col>
+            <el-col :span="5" style="background-color: #fff;height: 100%;margin-right: -10px;">
+                <div class="form-components">
+                    <span style="text-align: center;margin-top: 30px;">组件设置</span>
+                    <!--<ul>-->
+                    <!--<li prop="标题">-->
+                    <!--标题-->
+                    <!--<el-input></el-input>-->
+                    <!--</li>-->
+                    <!--<li prop="必填项目">-->
+                    <!--必填项目-->
+                    <!--<el-checkbox label="这个是必填项" name="type"></el-checkbox>-->
+                    <!--</li>-->
+                    <!--</ul>-->
+                    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign"
+                             styele="margin-top:30px;">
+                        <el-form-item label="标题">
+                            <el-input v-model="formLabelAlign.name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="必填项目">
+                            <el-checkbox v-model="formLabelAlign.must" label="这个是必填项" name="must">
+                            </el-checkbox>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </el-col>
+        </el-row>
     </section>
 </template>
 
@@ -48,35 +58,24 @@
     .createForm {
         width: 100%;
         min-height: 100%;
-        position: relative;
-        padding: 24px;
-        background: #fff;
-        .button{
+        font-size: 14px;
+        .button {
             background-color: #fff;
             margin-bottom: 20px;
             padding: 5px;
             /*padding-bottom: 5px;*/
-            text-align:right;
+            text-align: right;
         }
         .form-components {
             text-align: center;
             padding-top: 10px;
         }
-        ul{
-            margin-top: 10px;
+        ul {
+            margin-top: 30px;
             text-align: center;
         }
-        ul>li{
+        ul > li {
             margin-bottom: 10px;
-        }
-
-        .page {
-            width: 100%;
-            padding: 15px 30px;
-            text-align: right;
-            position: absolute;
-            bottom: 0;
-            right: 0;
         }
     }
 </style>
@@ -85,6 +84,11 @@
     export default {
         data() {
             return {
+                labelPosition: 'top',
+                formLabelAlign: {
+                    name: '',
+                    must: ''
+                },
                 btnLists: [
                     {
                         data: '单行文本框'
@@ -102,14 +106,17 @@
                         data: '上传'
                     }
                 ],
-                pluginslists:[
+                pluginslists: [
                     {
-                        title:'标题',
+                        title: '标题',
 
                     }
-                ]
+                ],
+                type: []
             }
         },
-        methods: {}
+        methods: {
+            drap: {}
+        }
     }
 </script>
