2e429c6b4de4a4e4f362a90c6050ff050a4332e8
wang-pengju <526527853@qq.com>
2017-10-13 09:37:58 +0800

    wang-pengju fixed some bug

--- src/common/js/filter.js
+++ src/common/js/filter.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-// import moment from 'moment'
+import moment from 'moment'
 
 Vue.filter('formatDate', (value, formatString) => {
     formatString = formatString || 'YYYY-MM-DD'
