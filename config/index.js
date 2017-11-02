f23928f490764e8bcfbac94bf445ab8a44e0297a
wang-pengju <526527853@qq.com>
2017-11-02 18:51:36 +0800

    wang-pengju fixed some bug

--- config/index.js
+++ config/index.js
@@ -29,13 +29,6 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-<<<<<<< HEAD
-                // target: 'http://192.168.0.124:9091', //平台
-                target:'http://192.168.0.102:9091',//平台
-=======
-                // target: 'http://192.168.0.102:9091', //平台
-                // target:'http://192.168.0.191:9092',//平台
->>>>>>> a0b8abc65ac74ecf1d9e2655c2e147b409a1373b
                 // target: 'http://192.168.0.198:9091',
                 // target: 'http://18f46963j4.iok.la',
                 // target:'http://192.168.0.136:9091',// 文军凡IP
