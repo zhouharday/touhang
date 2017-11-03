30336136a3901bda29073ed0f630b3d45337cab9
Harday-zh <719588366@qq.com>
2017-11-03 15:40:59 +0800

    update

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -123,12 +123,13 @@
             </el-menu>
             <!-- 菜单列表 Start -->
             <el-menu v-show="showOrHide.isShowSidebar && showOrHide.isVshowYe && isShow" :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router @open="handleOpen" @close="handleClose">
-              <el-submenu  :index="index" v-for="(item,index) in menus" :key="item">
+              <el-submenu :index="index" v-for="(item,index) in menus" :key="item">
                     <template slot="title" style="height:50px;line-height:50px;" class="title">
-                        <img style="margin-top: 18px;display: block;float: left;margin-right: 7px;" src="/static/img/office.png" />
-                        <span >{{item.menuName}}</span>
+                        <img style="margin-top:18px;display:block;float:left;margin-right:7px;" :key="index" :src="menusImg[index].src" />
+                        <span v-if="item.children.length !== 0">{{item.menuName}}</span>
+                        <span style="display: block;" :index="item.url" @click="addTab(item.menuName,'/home/' + item.url, item.url)" v-if="item.children.length == 0">{{item.menuName}}</span>
                     </template>
-                    <el-menu-item v-for="(item,index) in item.children" :key="item" :index="item.url" @click="addTab(item.menuName,'/home/' + item.url, item.url)">{{item.menuName}}</el-menu-item>
+                    <el-menu-item v-if="item.children.length !== 0" v-for="(item,index) in item.children" :key="item" :index="item.url" @click="addTab(item.menuName,'/home/' + item.url, item.url)">{{item.menuName}}</el-menu-item>
                 </el-submenu>
             </el-menu>
             <!-- <div index='2' v-if="item.children.length == 0" v-show="showOrHide.isShowSidebar && showOrHide.isVshowYe && isShow" v-for="(item,index) in menus" :key="item" class="div_el-menu-itemel-submenu__title" @click="addTab(item.menuName,'/home/' + item.url, item.url)">
@@ -187,6 +188,16 @@ export default {
   },
   data() {
     return {
+      menusImg: [ //菜单图标
+        { src: '/static/img/office.png'},
+        { src: '/static/img/zhushou.png'},
+        { src: '/static/img/project.png'},
+        { src: '/static/img/project_manger.png'},
+        { src: '/static/img/touzizhe.png'},
+        { src: '/static/img/survey.png'},
+        { src: '/static/img/sys_statistics.png'},
+        { src: '/static/img/sys_mang.png'},
+      ],
       menus: [],
       isShow: true,
       // isShow: false,
