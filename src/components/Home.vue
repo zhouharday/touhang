9c13bce39a46db8f1c885a009ef2c4dce6fcdc23
Harday-zh <719588366@qq.com>
2017-11-03 11:55:54 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,47 +14,43 @@
 </template>
 <style lang="less" scoped>
 .bounce-enter-active {
-    // animation: bounce-in 0.3s;
+  // animation: bounce-in 0.3s;
 }
 
 .bounce-leave-active {
-    // animation: bounce-in 0.3s reverse;
+  // animation: bounce-in 0.3s reverse;
 }
 @keyframes bounce-in {
-    0% {
-        transform: scale(0);
-    }
-    //   50% {
-    //     transform: scale(.5);
-    //   }
-    100% {
-        transform: scale(1);
-    }
+  0% {
+    transform: scale(0);
+  }
+  //   50% {
+  //     transform: scale(.5);
+  //   }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
 
 <script>
-import {
-    mapState
-} from 'vuex'
-import vHead from './Header.vue';
-import vSidebar from './Sidebar.vue';
-import VTab from '../views/home/homeTab.vue';
+import { mapState } from "vuex";
+import vHead from "./Header.vue";
+import vSidebar from "./Sidebar.vue";
+import VTab from "../views/home/homeTab.vue";
 export default {
-    components: {
-        vHead,
-        vSidebar,
-        VTab
-    },
-    computed: {
-        ...mapState({
-            isShow: state => state.login.showOrHide
-        })
-    },
-    data() {
-        return {
-
-        }
-    }
-}
+  components: {
+    vHead,
+    vSidebar,
+    VTab
+  },
+  computed: {
+    ...mapState({
+      isShow: state => state.login.showOrHide
+    })
+  },
+  data() {
+    return {};
+  }
+};
 </script>
