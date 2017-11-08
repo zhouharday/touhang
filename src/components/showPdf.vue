09d7827304df2be2d4848c254a549a318be97480
Harday-zh <719588366@qq.com>
2017-11-08 11:36:54 +0800

    update

--- src/components/showPdf.vue
+++ src/components/showPdf.vue
@@ -2,22 +2,13 @@
 <div class="cpdf">
     <div class="center">
         <div class="contor">
-            <el-button @click="prev" :disabled="!hasPrev">上一页</el-button>
-            <el-button @click="next" :disabled="!hasNext">下一页</el-button>
-            <span>Page: <span v-text="page_num"></span> / <span v-text="page_count"></span></span>
-
-            <el-button @click="addscale" icon="plus"></el-button>
-            <el-button @click="minus" icon="minus"></el-button>
             <el-button id="prev" @click="closepdf">关闭</el-button>
         </div>
-        <canvas id="myCanvas"></canvas>
     </div>
+    <iframe v-if="isshowpdf" :src="'../../../../static/js/pdf/web/viewer.html?file='+transUrl" width="100%" height="100%" scrolling="no"></iframe>
 </div>
 </template>
 <script>
-import PDFJS from '../../static/js/pdf/pdf.js'
-// import '../../static/js/pdf/pdf.worker.js'
-// import PDFJS from 'pdfjs'
 import {
     mapActions,
     mapGetters
@@ -33,131 +24,22 @@ export default {
         'pdfurl':function (to,from){
             if(to != from){
                 this.transUrl = to.replace('http://47.90.120.190:8086', '/file');
-                this.init();
+                this.isshowpdf = true;
             }
         }
     },
     data() {
         return {
-            transUrl: '',
-            pdfDoc: null, //pdfjs 生成的对象
-            pageNum: 1, //
-            pageRendering: false,
-            pageNumPending: null,
-            hasPrev: false,
-            hasNext: false,
-            scale: 1.2, //放大倍数
-            page_num: 0, //当前页数
-            page_count: 0, //总页数
-            maxscale: 2, //最大放大倍数
-            minscale: 0.8 //最小放大倍数
+            isshowpdf: false,
+            transUrl: ''
         }
     },
     methods: {
-        init() {
-            let vm = this;
-            console.log("预览地址：" + vm.transUrl);
-            if (vm.transUrl == '') {
-                return;
-            }
-            PDFJS.getDocument(vm.transUrl).then(function(pdfDoc_) { //初始化pdf
-                vm.pdfDoc = pdfDoc_;
-                vm.page_count = vm.pdfDoc.numPages;
-
-                vm.renderPage(vm.pageNum);
-            });
-        },
-        renderPage(num) { //渲染pdf
-            let vm = this
-            this.pageRendering = true;
-            let canvas = document.getElementById('myCanvas')
-            // Using promise to fetch the page
-            this.pdfDoc.getPage(num).then(function(page) {
-                var viewport = page.getViewport(vm.scale);
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Render PDF page into canvas context
-                var renderContext = {
-                    canvasContext: vm.ctx,
-                    viewport: viewport
-                };
-                var renderTask = page.render(renderContext);
-
-                // Wait for rendering to finish
-                renderTask.promise.then(function() {
-                    vm.pageRendering = false;
-                    if (vm.pageNumPending !== null) {
-                        // New page rendering is pending
-                        vm.renderPage(vm.pageNumPending);
-                        vm.pageNumPending = null;
-                    }
-                }).catch(err =>{
-                    console.log('renderTask error ', err);
-                });
-            }).catch(e => {
-                console.log('renderPage error ', e);
-                this.$emit('pdferr');
-            });
-            vm.page_num = vm.pageNum;
-
-            vm.hasPrev = vm.pageNum > 1;
-            vm.hasNext = vm.page_count > vm.pageNum;
-
-        },
-        addscale() { //放大
-            if (this.scale >= this.maxscale) {
-                return false;
-            }
-            this.scale += 0.1;
-            this.queueRenderPage(this.pageNum)
-        },
-        minus() { //缩小
-            if (this.scale <= this.minscale) {
-                return false;
-            }
-            this.scale -= 0.1;
-            this.queueRenderPage(this.pageNum)
-        },
-        prev() { //上一页
-            let vm = this
-            if (vm.pageNum <= 1) {
-                return false;
-            }
-            vm.pageNum--;
-            vm.queueRenderPage(vm.pageNum);
-        },
-        next() { //下一页
-            let vm = this
-            if (vm.pageNum >= vm.page_count) {
-                return false;
-            }
-            vm.pageNum++;
-            vm.queueRenderPage(vm.pageNum);
-        },
         closepdf() { //关闭PDF
-            this.pdfDoc = null;
-            this.page_count = 0;
-            this.pageNum = 1;
-
             this.$emit('closepdf');
-        },
-        queueRenderPage(num) {
-            if (this.pageRendering) {
-                this.pageNumPending = num;
-            } else {
-                this.renderPage(num);
-            }
-        }
-    },
-    computed: {
-        ctx() {
-            let id = document.getElementById('myCanvas')
-            return id.getContext('2d');
         }
     },
     mounted() {
-        this.init();
     }
 }
 </script>
@@ -180,7 +62,9 @@ export default {
         overflow: auto;
         padding-top: 20px;
         .contor {
-            margin-bottom: 10px;
+            position: absolute;
+            right: 50px;
+            top: 50px;
         }
     }
 }
