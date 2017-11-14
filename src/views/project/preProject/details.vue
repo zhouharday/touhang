e11dbd11866da883dc202306749f702b164f790e
PureMint <30465397+PureMint@users.noreply.github.com>
2017-11-14 17:24:30 +0800

    update
    冲突

--- src/views/project/preProject/details.vue
+++ src/views/project/preProject/details.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="form">
         <!-- 基本信息 -->
-<<<<<<< HEAD
         <tabel-header :data="checkProjectAuth('XQ-bianji')? headerInfo_basic : _headerInfo_basic" v-if="checkProjectAuth('XQ-bianji')" @add="disable(basicForm,headerInfo_basic)" @show="able(basicForm,headerInfo_basic)" @down="changeProjectInfo()" class="title"></tabel-header>
-=======
-        <tabel-header :data="checkProjectAuth('XQ-bianji')? headerInfo_basic : _headerInfo_basic" @add="disable(basicForm)" @show="changeProjectInfo()" class="title"></tabel-header>
->>>>>>> c7d9a594a6e3d2a73adc1cab8030bc9c620665f4
         <div class="basicForm">
             <el-form ref="basicForm" :model="basicForm" label-width="170px">
                 <el-row>
@@ -63,11 +59,7 @@
             </el-form>
         </div>
         <!-- 企业信息 -->
-<<<<<<< HEAD
         <tabel-header :data="checkProjectAuth('XQ-bianji')? headerInfo_company : _headerInfo_company" v-if="checkProjectAuth('XQ-bianji')" @add="disable(companyForm,headerInfo_company)" @show="able(companyForm,headerInfo_company)" @down="changeEnterpriseInfo()" class="title"></tabel-header>
-=======
-        <tabel-header :data="checkProjectAuth('XQ-bianji')? headerInfo_company : _headerInfo_company" @add="disable(companyForm)" @show="changeEnterpriseInfo()" class="title"></tabel-header>
->>>>>>> c7d9a594a6e3d2a73adc1cab8030bc9c620665f4
         <div class="companyForm">
             <el-form ref="companyForm" :model="companyForm" label-width="170px">
                 <el-row>
