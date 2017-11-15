7624a42efd2a5c53cc4151886600c84260f8166d
cj <cj635881197@126.com>
2017-11-15 17:25:49 +0800

    CJ 分页样式
    CJ 分页样式

--- src/views/project/aftProject/aftProject.vue
+++ src/views/project/aftProject/aftProject.vue
@@ -34,8 +34,8 @@
             </el-table>
         </div>
         <!-- 分页 -->
-        <div class="page">
-            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-sizes="[2, 3, 5, 10]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+        <div class="pagination">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-sizes="[10, 20, 30, 40]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
             </el-pagination>
         </div>
     </section>
