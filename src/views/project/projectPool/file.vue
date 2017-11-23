23ffd4ce3d8d694d0d5f07dedd1b9f30092dcaa6
cj <cj635881197@126.com>
2017-11-23 13:46:50 +0800

    cj

--- src/views/project/projectPool/file.vue
+++ src/views/project/projectPool/file.vue
@@ -30,7 +30,7 @@
                     </el-form-item>
                     <el-form-item label="上传文件">
                         <!-- action 上传的地址，必填 -->
-                        <Upload ref="upload" multiple type="drag":on-success="handleSuccess" :headers ='headers':data="uploadInfo" :action="actionUrl">
+                        <Upload ref="upload" multiple type="drag":on-success="handleSuccess" :headers ="headers":data="uploadInfo" :action="actionUrl">
                             <div style="padding: 20px 0">
                                 <Icon type="ios-cloud-upload" size="52"></Icon>
                                 <p>点击或将文件拖拽到这里上传</p>
