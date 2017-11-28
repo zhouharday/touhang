90fe39f48dc8016a960b6b3f99a1b602ae03fcad
Harday-zh <719588366@qq.com>
2017-11-28 18:23:46 +0800

    update

--- src/views/investor/investor.vue
+++ src/views/investor/investor.vue
@@ -6,7 +6,7 @@
             </el-input>
         </tableHeader>
         <div class="tables">
-            <el-table :data="investorData" v-loading="loading" style="width: 100%;">
+            <el-table :data="investorData" v-loading="loadingTab" style="width: 100%;">
                 <el-table-column label="投资者名称" align="center">
                     <template scope="scope">
                         <div class="name" @click="handleRouter(scope.$index, scope.row)">
@@ -40,10 +40,11 @@
         <!-- 添加投资者 -->
         <!-- :ref="addInvestor" -->
         <el-dialog title="新增投资者" :visible.sync="modelInvestor" :close-on-click-modal="false">
-            <investor-form :investorForm="addInvestor" ref="addInvestor"></investor-form>
+            <investor-form :investorForm="addInvestor" ref="addInvestor" :loading="loading"></investor-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="modelInvestor = false">取 消</el-button>
-                <el-button type="danger" @click="confirmIncome(addInvestor)">确 定</el-button>
+                <el-button :loading="loading" type="danger" @click="confirmIncome(addInvestor)">确 定</el-button>
+                <!-- <Button :loading="loading" type="button" @click="confirmIncome(addInvestor)">确 定</Button> -->
             </div>
         </el-dialog>
         <!-- 确认删除模态框 -->
@@ -60,271 +61,293 @@
 </template>
 
 <script type="text/ecmascript-6">
-import {mapMutations, mapGetters} from 'vuex'
-import investorForm from 'components/investorForm'
-import myFilter from 'components/myFilter'
-import tableHeader from 'components/tabelHeader'
-import deleteReminders from 'components/deleteReminders'
-import protocolDetails from './protocolDetails'
+import { mapMutations, mapGetters, mapState } from "vuex";
+import investorForm from "components/investorForm";
+import myFilter from "components/myFilter";
+import tableHeader from "components/tabelHeader";
+import deleteReminders from "components/deleteReminders";
+import protocolDetails from "./protocolDetails";
 import {
-    addInvestor,
-    getInvestorType,
-    deleteInvestor,
-    addAgreement,
-    getInvestorList
-} from 'api/investor'
+  addInvestor,
+  getInvestorType,
+  deleteInvestor,
+  addAgreement,
+  getInvestorList
+} from "api/investor";
 export default {
-    data() {
-        return {
-            chooseInfo: {
-                title: '投资者类型：',
-                details: [{
-                    id: '',
-                    dicName: '全部'
-                }]
-            },
-            theme: '#fff',
-            dataTitle: {
-                btnGroup: [{
-                    icon: 'plus-round',
-                    explain: '投资者'
-                }]
-            },
-            deleteReminders: false,
-            id: '',
-            modelSign: false,
-            modelInvestor: false,
-            signInfo: {
-                agreementName: '',
-                structuralLevelId: '',
-                investorName: '',
-                inverstorId: '',
-                fundId: '',
-                subscribeAmount: '',
-                signDate: '',
-                // regionName: '',
-                managerName: '',
-                merchantId: JSON.parse(sessionStorage.getItem('merchants'))[0].id,
-                registerDate: new Date()
-            },
-            investorData: [],
-            loading: true,
-            addInvestor: {
-                investorName: '',
-                investorTypeId: '',
-                certificateTypeId: '',
-                certificateNum: '',
-                organizationProperty: '',
-                personalAssets: '',
-                regionId: '',
-                regionName: '',
-                investmentManagerId: '',
-                contacts: '',
-                contactNumber: '',
-                addUserId: JSON.parse(sessionStorage.getItem('userInfor')).id,
-                merchantId: JSON.parse(sessionStorage.getItem('merchants'))[0].id,
-                address: '',
-                remark: ''
-            },
-            investorId: '', // 用于模糊查询 投资者类型ID
-            investorName: '', // input 输入框值
-            pageTotal: '',
-            page: 1,
-            pageSize: 10
+  data() {
+    return {
+      dbClick: false,
+      chooseInfo: {
+        title: "投资者类型：",
+        details: [
+          {
+            id: "",
+            dicName: "全部"
+          }
+        ]
+      },
+      theme: "#fff",
+      dataTitle: {
+        btnGroup: [
+          {
+            icon: "plus-round",
+            explain: "投资者"
+          }
+        ]
+      },
+      deleteReminders: false,
+      id: "",
+      modelSign: false,
+      modelInvestor: false,
+      signInfo: {
+        agreementName: "",
+        structuralLevelId: "",
+        investorName: "",
+        inverstorId: "",
+        fundId: "",
+        subscribeAmount: "",
+        signDate: "",
+        // regionName: '',
+        managerName: "",
+        merchantId: JSON.parse(sessionStorage.getItem("merchants"))[0].id,
+        registerDate: new Date()
+      },
+      investorData: [],
+      loadingTab: true,
+      addInvestor: {
+        investorName: "",
+        investorTypeId: "",
+        certificateTypeId: "",
+        certificateNum: "",
+        organizationProperty: "",
+        personalAssets: "",
+        regionId: "",
+        regionName: "",
+        investmentManagerId: "",
+        contacts: "",
+        contactNumber: "",
+        addUserId: JSON.parse(sessionStorage.getItem("userInfor")).id,
+        merchantId: JSON.parse(sessionStorage.getItem("merchants"))[0].id,
+        address: "",
+        remark: ""
+      },
+      investorId: "", // 用于模糊查询 投资者类型ID
+      investorName: "", // input 输入框值
+      pageTotal: "",
+      page: 1,
+      pageSize: 10
+    };
+  },
+  methods: {
+    handleRouter(index, rowList) {
+      this.addTab({
+        type: "addTab",
+        title: rowList.investorName + "详情页",
+        url: "/home/investorDetails/" + rowList.id,
+        name: "investorDetails/" + rowList.id
+      });
+      this.GET_INVESTORNAME(rowList.investorName);
+      this.$router.push({
+        name: "investorDetails",
+        params: {
+          userId: rowList.id
         }
+      });
+      sessionStorage.setItem("INVESTORNAME", rowList.investorName);
     },
-    methods: {
-        handleRouter(index, rowList) {
-            this.addTab({
-                type: 'addTab',
-                title: rowList.investorName + '详情页',
-                url: '/home/investorDetails/' + rowList.id,
-                name: 'investorDetails/' + rowList.id
-            })
-            this.GET_INVESTORNAME(rowList.investorName)
-            this.$router.push({
-                name: 'investorDetails',
-                params: {
-                    userId: rowList.id
-                }
-            })
-            sessionStorage.setItem('INVESTORNAME', rowList.investorName)
-        },
-        showModel() {
-            let new_addInvestor = {
-                investorName: '',
-                investorTypeId: '',
-                certificateTypeId: '',
-                certificateNum: '',
-                organizationProperty: '',
-                personalAssets: '',
-                regionId: '',
-                regionName: '',
-                investmentManagerId: JSON.parse(sessionStorage.getItem('userInfor')).id,
-                investmentManagerName: JSON.parse(sessionStorage.getItem('userInfor')).name,
-                contacts: '',
-                contactNumber: '',
-                addUserId: JSON.parse(sessionStorage.getItem('userInfor')).id,
-                merchantId: JSON.parse(sessionStorage.getItem('merchants'))[0].id,
-                address: '',
-                remark: ''
-            };
-            this.addInvestor = new_addInvestor
-            this.modelInvestor = true;
-        },
-        handleDelete(index, row) {
-            this.deleteReminders = !this.deleteReminders
-            this.id = row.id
-        },
-        handleEdit(index, row) {
-            this.modelSign = true
-            this.signInfo.agreementName = row.agreementName
-            this.signInfo.signDate = new Date(row.createDate)
-            this.signInfo.inverstorId = row.id
-            this.signInfo.managerName = row.investmentManagerName
-            this.signInfo.investorName = row.investorName
-        },
-        confirmIncome(formName) {
-            // console.log(this.showModel())/
-            this.$refs.addInvestor.$refs.addInvestor.validate((valid) => {
-                if (valid) {
-                    addInvestor(this.addInvestor).then((res) => {
-                        if (res.data.status == '200') {
-                            this.modelInvestor = false
-                            this.getInvList()
-                            this.addInvestor = {}
-                        }
-                    })
-                } else {
-                    return false
-                }
-            })
-        },
-        comfirmDel() {
-            deleteInvestor(this.id).then((res) => {
-                if (res.status == '200') {
-                    this.$Message.success(res.data.message || '删除投资者成功！')
-                    this.deleteReminders = false
-                    this.getInvList()
-                }
-            })
-        },
-        confirmSign() {
-            var agreementInfo = this.$refs.protocolInfo.$refs.AgreementInfo
-            agreementInfo.validate((valid) => {
-                if (valid) {
-                    addAgreement(this.signInfo).then((res) => {
-                        if (res.status == '200') {
-                            this.$Message.success(res.data.message || '签约成功！')
-                            this.modelSign = false
-                            this.signInfo = {}
-                        }
-                    })
-                } else {
-                    this.$Message.error('表单验证失败!')
-                    return false
-                }
-            })
-        },
-        cancelSign() {
-            var agreementInfo = this.$refs.protocolInfo.$refs.AgreementInfo
-            agreementInfo.resetFields()
-            this.modelSign = false
-        },
-        comfirmCancel() {
-            this.deleteReminders = false
-        },
-        handleSizeChange(val) {
-            // console.log(`每页 ${val} 条`);
-            this.pageSize = val
-            this.getInvList()
-        },
-        handleCurrentChange(val) {
-            // console.log(`当前页: ${val}`);
-            this.page = val
-            this.getInvList()
-        },
-        investorSearch() {
-            this.getInvList()
-        },
-        investorFilter(index, id) {
-            if (index == 0) {
-                this.investorId = ''
-            } else {
-                this.investorId = id
+    showModel() {
+      let new_addInvestor = {
+        investorName: "",
+        investorTypeId: "",
+        certificateTypeId: "",
+        certificateNum: "",
+        organizationProperty: "",
+        personalAssets: "",
+        regionId: "",
+        regionName: "",
+        investmentManagerId: JSON.parse(sessionStorage.getItem("userInfor")).id,
+        investmentManagerName: JSON.parse(sessionStorage.getItem("userInfor"))
+          .name,
+        contacts: "",
+        contactNumber: "",
+        addUserId: JSON.parse(sessionStorage.getItem("userInfor")).id,
+        merchantId: JSON.parse(sessionStorage.getItem("merchants"))[0].id,
+        address: "",
+        remark: ""
+      };
+      this.addInvestor = new_addInvestor;
+      this.loading = false;
+      this.modelInvestor = true;
+    },
+    handleDelete(index, row) {
+      this.deleteReminders = !this.deleteReminders;
+      this.id = row.id;
+    },
+    handleEdit(index, row) {
+      this.modelSign = true;
+      this.signInfo.agreementName = row.agreementName;
+      this.signInfo.signDate = new Date(row.createDate);
+      this.signInfo.inverstorId = row.id;
+      this.signInfo.managerName = row.investmentManagerName;
+      this.signInfo.investorName = row.investorName;
+    },
+    confirmIncome(formName) {
+      // console.log(this.showModel());
+      this.$refs.addInvestor.$refs.addInvestor.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          console.log(this.loading);
+          addInvestor(this.addInvestor).then(res => {
+            if (res.data.status == "200") {
+              this.modelInvestor = false;
+            //   this.loading = false;
+              this.getInvList();
+              this.addInvestor = {};
             }
-            this.getInvList()
-        },
-        getInvList() {
-            getInvestorList(this.investorId, this.investorName, this.page, this.pageSize).then((res) => {
-                if (res.status == '200') {
-                    console.log(res)
-                    this.investorData = res.data.result.list
-                    this.pageTotal = res.data.result.total
-                }
-            })
-        },
-        ...mapMutations([
-            'addTab', // 映射 this.addTab() 为 this.$store.commit
-            'GET_INVESTORNAME'
-        ])
+          });
+        } else {
+          return false;
+        }
+      });
     },
-    created() {
-        this.$store.dispatch('getInvestor').then(() => {
-            this.investorData = this.investorList.list
-            this.pageTotal = this.investorList.total
-            this.loading = false
-        })
-        getInvestorType().then((res) => {
-            if (res.status == '200') {
-                this.chooseInfo.details = this.chooseInfo.details.concat(res.data.result)
+    comfirmDel() {
+      deleteInvestor(this.id).then(res => {
+        if (res.status == "200") {
+          this.$Message.success(res.data.message || "删除投资者成功！");
+          this.deleteReminders = false;
+          this.getInvList();
+        }
+      });
+    },
+    confirmSign() {
+      var agreementInfo = this.$refs.protocolInfo.$refs.AgreementInfo;
+      agreementInfo.validate(valid => {
+        if (valid) {
+          addAgreement(this.signInfo).then(res => {
+            if (res.status == "200") {
+              this.$Message.success(res.data.message || "签约成功！");
+              this.modelSign = false;
+              this.signInfo = {};
             }
-        }).catch(err => {
-            this.$Message.error(err)
-        })
+          });
+        } else {
+          this.$Message.error("表单验证失败!");
+          return false;
+        }
+      });
     },
-    computed: {
-        ...mapGetters([
-            'investorList'
-        ])
+    cancelSign() {
+      var agreementInfo = this.$refs.protocolInfo.$refs.AgreementInfo;
+      agreementInfo.resetFields();
+      this.modelSign = false;
     },
-    components: {
-        myFilter,
-        tableHeader,
-        investorForm,
-        deleteReminders,
-        protocolDetails
-    }
-}
+    comfirmCancel() {
+      this.deleteReminders = false;
+    },
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`);
+      this.pageSize = val;
+      this.getInvList();
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`);
+      this.page = val;
+      this.getInvList();
+    },
+    investorSearch() {
+      this.getInvList();
+    },
+    investorFilter(index, id) {
+      if (index == 0) {
+        this.investorId = "";
+      } else {
+        this.investorId = id;
+      }
+      this.getInvList();
+    },
+    getInvList() {
+      getInvestorList(
+        this.investorId,
+        this.investorName,
+        this.page,
+        this.pageSize
+      ).then(res => {
+        if (res.status == "200") {
+          console.log(res);
+          this.investorData = res.data.result.list;
+          this.pageTotal = res.data.result.total;
+        }
+      });
+    },
+    ...mapMutations([
+      "addTab", // 映射 this.addTab() 为 this.$store.commit
+      "GET_INVESTORNAME"
+    ])
+  },
+  computed: {
+    ...mapState({
+      loading: state => state.login.loading
+    })
+  },
+  created() {
+    this.$store.dispatch("getInvestor").then(() => {
+      this.investorData = this.investorList.list;
+      this.pageTotal = this.investorList.total;
+      this.loadingTab = false;
+    });
+    getInvestorType()
+      .then(res => {
+        if (res.status == "200") {
+          this.chooseInfo.details = this.chooseInfo.details.concat(
+            res.data.result
+          );
+        }
+      })
+      .catch(err => {
+        this.$Message.error(err);
+      });
+  },
+  computed: {
+    ...mapGetters(["investorList"])
+  },
+  components: {
+    myFilter,
+    tableHeader,
+    investorForm,
+    deleteReminders,
+    protocolDetails
+  }
+};
 </script>
 
 <style lang="less" scoped>
-@import '../../common/styles/variable.less';
-@import '../../common/styles/mixin.less';
+@import "../../common/styles/variable.less";
+@import "../../common/styles/mixin.less";
 .investor {
+  width: 100%;
+  // min-height: 100%;
+  padding: 24px;
+  background: @color-base;
+  position: relative;
+  /*.table-router();*/
+  .addPadding {
+    padding-bottom: 12px;
+  }
+  .tables {
+    // padding-bottom: @height-large;
+  }
+  .page {
     width: 100%;
-    // min-height: 100%;
-    padding: 24px;
-    background: @color-base;
-    position: relative;
-    /*.table-router();*/
-    .addPadding {
-        padding-bottom: 12px;
-    }
-    .tables {
-        // padding-bottom: @height-large;
-    }
-    .page {
-        width: 100%;
-        height: 60px;
-        text-align: right;
-        position: absolute;
-        bottom: 0;
-        right: 0;
-    }
-    span.investorName {
-        cursor: pointer;
-        color: #f05e5e;
-    }
+    height: 60px;
+    text-align: right;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+  span.investorName {
+    cursor: pointer;
+    color: #f05e5e;
+  }
 }
 </style>
