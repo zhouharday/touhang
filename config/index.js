ac1b3dfd340c6835dce820e0cdebcdda6aa1a385
Harday-zh <719588366@qq.com>
2017-11-28 16:36:09 +0800

    update

--- config/index.js
+++ config/index.js
@@ -32,10 +32,10 @@ module.exports = {
                 // target: 'http://192.168.0.198:9091',
                 // target: 'http://18f46963j4.iok.la',
                 // target: 'http://192.168.0.118:9091', // 文军凡IP
-                // target: 'http://192.168.0.118:9091', // 文格阳IP
+                target: 'http://192.168.0.118:9091', // 文格阳IP
                 // target: 'http://192.168.0.105:9091', // 蔡永亮 IP
                 // target:'http://100.64.33.181:9091',
-                target: 'http://sdwlyxgs.imwork.net:16380',
+                // target: 'http://sdwlyxgs.imwork.net:16380',
                 changeOrigin: true,
                 secure: false,
                 pathRewrite: {
